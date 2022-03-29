a36d94fd868194495ddb6e3d58669974a0c1f01d
Yulia <yudannikova@gmail.com>
2022-03-29 11:24:27 +0200

    Mistakes corrected

--- genome.py
+++ genome.py
@@ -1,11 +1,11 @@
 genome = input()
-prev_letter = [0]
+prev_letter = genome[0]
 count = 1
-for letter in genome:
+for letter in genome[1:]:
   if prev_letter == letter:
     count += 1
   else:
-    print(str(prev_letter) + str(count), end = '')
+    print(prev_letter + str(count), end = '')
     count = 1
     prev_letter = letter
-print(str(prev_letter) + str(count))
+print(prev_letter + str(count))
